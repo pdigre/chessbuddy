b0732d0a3b62d1c7fcd60aea24794eaf45a9ff42
per.digre <per.digre@aize.io>
2022-03-11 21:06:51 +0100

    Fix GCP issues

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const server = express();
 const path = require('path');
 const port = process.env.PORT;
 
-const staticFiles = path.join(__dirname, '../build');
+const staticFiles = path.join(__dirname, 'build');
 server.use(express.static(staticFiles));
 server.listen(port);
 console.log(`Running ChessBuddy from ${staticFiles} on port ${port}`);
