e8018ce8bd1bf6e9373273ce050f94b08af0822e
per.digre <per.digre@aize.io>
2022-03-13 01:51:42 +0100

    Cleanup

--- server/server.js
+++ server/server.js
@@ -4,12 +4,8 @@ const express = require('express');
 const server = express();
 const path = require('path');
 const port = process.env.PORT;
-const compression = require('compression');
-const helmet = require('helmet');
 
 const staticFiles = path.join(__dirname, 'build');
-server.use(compression());
-server.use(helmet());
 server.use(express.static(staticFiles));
 server.listen(port);
 console.log(`Running ChessBuddy from ${staticFiles} on port ${port}`);
