c4756c728d2389f57371c211de028170531ff99d
per.digre <per.digre@aize.io>
2023-02-11 20:10:33 +0100

    small fixes

--- src/model/engine.ts
+++
@@ -1,8 +0,0 @@
-export class Engine {
-  constructor(public name: string, public path: string) {}
-}
-
-export const Engines: Engine[] = [
-  new Engine('Stockfish', 'bots/stockfish.js-10.0.2/stockfish.js'),
-  new Engine('Lozza', 'bots/lozza-1.18/lozza.js'),
-];
