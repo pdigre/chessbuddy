5aac7fbb259d32b9c76b30fcec6149a20c74edb2
per.digre <per.digre@aize.io>
2023-02-22 23:22:59 +0100

    Fix error handling

--- src/service/bluetooth.service.ts
+++ src/service/bluetooth.service.ts
@@ -1,15 +1,22 @@
+import { messageService } from './index.service';
+
 export class BluetoothService {
   btDevices: BluetoothDevice[] = [];
 
   constructor() {
-    navigator.bluetooth
-      .getDevices()
-      .then(devices => {
-        devices.forEach(device => {
-          this.btDevices.push(device);
-        });
-      })
-      .catch(error => console.log('Argh! ' + error));
+    try {
+      const bt = navigator?.bluetooth;
+      const devs = bt.getDevices();
+      devs
+        ?.then(devices => {
+          devices.forEach(device => {
+            this.btDevices.push(device);
+          });
+        })
+        .catch(error => console.log('Argh! ' + error));
+    } catch (error) {
+      messageService.display('Bluetooth error:', String(error));
+    }
   }
 }
 
