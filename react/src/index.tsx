355099a248328d49195330c68f503d01722084c2
per.digre <per.digre@aize.io>
2023-12-05 16:55:33 +0100

    Fixed scripts

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './main.css';
-import App from './src/App';
+import App from './App';
 
 const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
