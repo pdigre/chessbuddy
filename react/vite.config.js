c8381dbec8076c9c8d0aaadf4bfaebb5c64f5dc8
per.digre <per.digre@aize.io>
2025-01-29 23:13:28 +0100

    Fixes for tailwindcss

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   publicDir: "../public",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 })
