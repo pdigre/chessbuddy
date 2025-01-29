d9f243c438e3d813c9da6c48b6f12201c41c8abc
per.digre <per.digre@aize.io>
2025-01-29 22:10:23 +0100

    Fixes for debugging with Vite

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-    publicDir: "../public",
-    plugins: [tsconfigPaths()],
+  publicDir: "../public",
+  plugins: [react(), tsconfigPaths()],
 })
