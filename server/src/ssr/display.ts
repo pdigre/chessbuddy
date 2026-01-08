56ca33b2ff322ab3eea0acd5dcdb447f6d6f789a
Per Digre <per@digre.com>
2026-01-08 14:59:55 +0100

    Db 5 (#190)
    * Firestore data - 5
    
    * Firestore data - 5
    
    * Firestore data - 5
    
    ---------
    
    Co-authored-by: Per Digre <per.digre@bouvet.no>

---
+++ server/src/ssr/display.ts
@@ -0,0 +1,19 @@
+import { type GoogleUserInfo } from '../googleAuth.ts';
+import { readData } from '../datastore.ts';
+
+export default async function helloPage(req: Request, user: GoogleUserInfo) {
+  const userData = await readData(user.email);
+  return {
+    title: 'Hello World',
+    body: `
+        <script src="https://cdn.jsdelivr.net/npm/renderjson@1.4.0/renderjson.min.js"></script>
+        <h2>Server data for ${user.email}</h2>
+        <link rel="stylesheet" href="./display.css">
+        <div id="data-tree"></div>
+        <script type="module">
+            import { loadJson } from './display.js';
+            loadJson(${JSON.stringify(userData)});
+        </script>
+      `,
+  };
+}
